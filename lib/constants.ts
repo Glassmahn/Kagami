0c68d48f46da4907f7fbccdaa0c3b3998ec165cf
Glassmahn <glassman4664@gmail.com>
2026-05-07 02:56:51 +0100

    feat: add KagamiCore ABI and contract address constants

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const KAGAMI_CORE_ADDRESS = process.env.NEXT_PUBLIC_KAGAMI_CORE_ADDRESS as `0x${string}`
+export const SEED_FACTORY_ADDRESS = process.env.NEXT_PUBLIC_SEED_FACTORY_ADDRESS as `0x${string}`
+export const KAGAMI_TOKEN_ADDRESS = process.env.NEXT_PUBLIC_KAGAMI_TOKEN_ADDRESS as `0x${string}`
