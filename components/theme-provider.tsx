cbaab901c99e069c7e880a7bf562c98de2afbc48
Glassmahn <glassman4664@gmail.com>
2026-05-07 01:34:27 +0100

    feat: enforce dark mirror theme with forced dark mode

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -7,5 +7,14 @@ import {
 } from 'next-themes'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider 
+      {...props} 
+      defaultTheme="dark" 
+      forcedTheme="dark"
+      attribute="class"
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
